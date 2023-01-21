39d48ff2c8a284ef99596959d11b95cad8ceaca8
Jonathan Zrake <jonathan.zrake@gmail.com>
2023-01-20 21:25:11 -0500

    Initial Commit

---
+++ Kepler/KeplerApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeplerApp.swift
+//  Kepler
+//
+//  Created by Jonathan Zrake on 1/20/23.
+//
+
+import SwiftUI
+
+@main
+struct KeplerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
